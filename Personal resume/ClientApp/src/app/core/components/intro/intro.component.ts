700046942f0f7182dd26952463045b6e97045439
astritdemiri11 <astritdemiri11@gmail.com>
2022-07-15 02:39:47 +0200

    Intro social media

--- Personal resume/ClientApp/src/app/core/components/intro/intro.component.ts
+++ Personal resume/ClientApp/src/app/core/components/intro/intro.component.ts
@@ -9,6 +9,13 @@ import { SharedService } from 'src/app/shared/services/shared.service';
 export class IntroComponent {
   constructor(private sharedService: SharedService) { }
 
+  socialMedias: { icon: string, name: string, url: string }[] = [
+    { name: 'LinkedIn', icon: 'social-media:linkedin', url: '' },
+    { name: 'Github', icon: 'social-media:github', url: '' },
+    { name: 'Instagram', icon: 'social-media:instagram', url: '' },
+    { name: 'Facebook', icon: 'social-media:facebook', url: '' },
+    { name: 'Twitter', icon: 'social-media:twitter', url: '' },
+  ]
   isHandset() {
     return this.sharedService.business.isHandset();
   }
